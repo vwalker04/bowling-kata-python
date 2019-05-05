846f701e0a5ffaafb888b604585af11e635edc7f
Vaughn Walker <vaughn.walker@integral.io>
2019-05-04 23:20:39 -0400

    Score a game of bowling

--- game_scoring.py
+++ game_scoring.py
@@ -1,11 +1,41 @@
-import random
-
-
 class Game:
+    def __init__(self):
+        self.total_score = 0
+        self.rolls = []
+
     def score(self):
-        score = 0
+        roll_index = 0
 
-        for x in range(10):
-            score = str(random.randint(0, 301))
+        for frame_index in range(10):
+            if self.__is_strike(roll_index):
+                self.total_score += self.__strike_score(roll_index)
+                roll_index += 1
+            elif self.__is_spare(roll_index):
+                self.total_score += self.__spare_score(roll_index)
+                roll_index += 2
+            else:
+                self.total_score += self.__roll_frame(roll_index)
+                roll_index += 2
 
-        return score
+        return self.total_score
+
+    def roll(self, pins_knocked_down):
+        self.rolls.append(pins_knocked_down)
+
+    def reset_game(self):
+        self.total_score = 0
+
+    def __roll_frame(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1]
+
+    def __strike_score(self, roll_index):
+        return 10 + self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
+
+    def __spare_score(self, roll_index):
+        return 10 + self.rolls[roll_index + 2]
+
+    def __is_strike(self, roll_index):
+        return self.rolls[roll_index] == 10
+
+    def __is_spare(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1] == 10
--- game_window.py
+++ game_window.py
@@ -8,15 +8,20 @@ from gi.repository import Gtk
 
 class App:
     def __init__(self):
-        builder = Gtk.Builder()
-        builder.add_from_file("bowling_display.glade")
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("bowling_display.glade")
 
         self.game = Game()
-        self.main_window = builder.get_object("main_window")
-        self.score_display = builder.get_object("scr_display")
+        self.main_window = self.builder.get_object("main_window")
+        self.score_display = self.builder.get_object("scr_display")
 
-        self.score_button = builder.get_object("scr_button")
+        self.score_button = self.builder.get_object("scr_button")
         self.score_button.connect("clicked", self.update_score)
+
+        self.reset_button = self.builder.get_object("reset_button")
+        self.reset_button.connect("clicked", self.reset_game)
+
+        self.rolls_from_builder = self.__build_roll_objects()
 
     def run(self):
         self.main_window.connect("destroy", Gtk.main_quit)
@@ -24,8 +29,36 @@ class App:
 
         Gtk.main()
 
-    def update_score(self, object):
-        self.score_display.set_text(self.game.score())
+    def update_score(self, score_button):
+        self.roll()
+        self.score_display.set_text(str(self.game.score()))
+
+    def roll(self):
+        rolls_input = self.__get_roll_input()
+
+        for pins_knocked_down in rolls_input:
+            if pins_knocked_down == "":
+                pins_knocked_down = 0
+            self.game.roll(int(pins_knocked_down))
+
+    def reset_game(self, reset_button):
+        ''' Reset should clear out current inputs
+            Reset should also zero out the current score
+            on both the display and in bowling logic '''
+        self.game.reset_game()
+
+    def __build_roll_objects(self):
+        rolls = []
+        incremented_rolls = ["roll_%s" % roll for roll in range(0, 21)]
+        for roll in incremented_rolls:
+            rolls.append(self.builder.get_object(roll))
+        return rolls
+
+    def __get_roll_input(self):
+        rolls = []
+        for roll in self.rolls_from_builder:
+            rolls.append(roll.get_text())
+        return rolls
 
 
 app = App()
